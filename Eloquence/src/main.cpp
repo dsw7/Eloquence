b74280111aad3f53094962e6ef9902a79edadcaf
David Weber <dds3443@hotmail.com>
2024-05-07 01:54:28 -0700

    Load file

--- Eloquence/src/main.cpp
+++ Eloquence/src/main.cpp
@@ -1,4 +1,22 @@
-int main()
+#include <filesystem>
+#include <iostream>
+
+int main(int argc, char **argv)
 {
-    return 0;
+    if (argc < 2)
+    {
+        std::cerr << "Usage: $ " << argv[0] << " /path/to/xml\n";
+        return EXIT_FAILURE;
+    }
+
+    std::string path_xml = std::string(argv[1]);
+    std::cout << "Loading '" << path_xml << "'\n";
+
+    if (not std::filesystem::exists(path_xml))
+    {
+        std::cerr << "Could not find " << path_xml << '\n';
+        return EXIT_FAILURE;
+    }
+
+    return EXIT_SUCCESS;
 }
