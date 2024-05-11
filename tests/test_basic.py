ef8a83520c87d730e9c17cc5b467cb6f13c2cc87
David Weber <dds3443@hotmail.com>
2024-05-11 05:40:05 -0700

    Add valid tests

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -3,11 +3,25 @@ from os import EX_OK
 from subprocess import run
 from pytest import mark
 
+VALID_FILES = [
+    "tests/xml/test_ping_scan.xml",
+]
 INVALID_FILES = [
     "tests/xml/test_broken_xml.xml",
     "tests/xml/test_html_input.xml",
     "tests/xml/test_non_nmap.xml",
 ]
+
+
+@mark.parametrize("xml_path", VALID_FILES)
+def test_valid(xml_path: Path, capfd) -> None:
+    process = run(["build/elo", xml_path])
+    assert process.returncode == EX_OK
+
+    cap = capfd.readouterr()
+    print()
+    print(cap.out)
+    print(cap.err)
 
 
 @mark.parametrize("xml_path", INVALID_FILES)
@@ -16,4 +30,6 @@ def test_invalid(xml_path: Path, capfd) -> None:
     assert process.returncode != EX_OK
 
     cap = capfd.readouterr()
+    print()
+    print(cap.out)
     print(cap.err)
