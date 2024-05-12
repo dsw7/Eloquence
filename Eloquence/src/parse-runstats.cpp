0fa6028ba4414472e6f691dbc4d1339fd60ae8a6
David Weber <dds3443@hotmail.com>
2024-05-12 03:11:25 -0700

    Add collection date

--- Eloquence/src/parse-runstats.cpp
+++ Eloquence/src/parse-runstats.cpp
@@ -12,13 +12,24 @@ void parse_hosts(xml_node *node_hosts)
     }
 
     xml_attr *total = node_hosts->first_attribute("total");
-    std::cout << std::setw(30) << std::left << "Number of machines scanned: " << total->value() << '\n';
+    std::cout << std::setw(30) << std::left << "Number of machines scanned:" << total->value() << '\n';
 
     xml_attr *down = node_hosts->first_attribute("down");
     std::cout << std::setw(30) << std::left << "Number of machines down:" << down->value() << '\n';
 
     xml_attr *up = node_hosts->first_attribute("up");
     std::cout << std::setw(30) << std::left << "Number of machines up:" << up->value() << '\n';
+}
+
+void parse_finished(xml_node *node_finished)
+{
+    if (node_finished == NULL)
+    {
+        return;
+    }
+
+    xml_attr *timestr = node_finished->first_attribute("timestr");
+    std::cout << std::setw(30) << std::left << "Report collected at:" << timestr->value() << '\n';
 }
 
 void get_runstats(xml_node *node)
@@ -29,4 +40,5 @@ void get_runstats(xml_node *node)
     }
 
     ::parse_hosts(node->first_node("hosts"));
+    ::parse_finished(node->first_node("finished"));
 }
