2e71f60b60f5ee11e58fb46460c10e4faf33e406
dsw7 <dds3443@hotmail.com>
2024-05-12 04:42:26 -0700

    Add help messages

--- Eloquence/src/main.cpp
+++ Eloquence/src/main.cpp
@@ -7,11 +7,30 @@
 #include <filesystem>
 #include <iostream>
 
+void help_messages()
+{
+    std::string name = std::string(PROJECT_NAME);
+    std::string doc = "\033[1mNAME:\033[0m\n"
+                      "  \033[4m" +
+                      name +
+                      "\033[0m\n\n"
+                      "\033[1mDESCRIPTION:\033[0m\n"
+                      "  A command line program for parsing Nmap XML files. To be used as\n"
+                      "  part of a larger workflow. See \033[4mhttps://github.com/dsw7/GPTifier\033[0m\n"
+                      "  for more information.\n\n"
+                      "\033[1mSYNOPSIS:\033[0m\n"
+                      "  \033[4melo\033[0m [-h | --help] </path/to/xml>\n\n"
+                      "\033[1mOPTIONS:\033[0m\n"
+                      "  \033[2m-h, --help\033[0m\n"
+                      "    Print help information and exit.\n\n";
+    std::cout << doc;
+}
+
 int main(int argc, char **argv)
 {
     if (argc < 2)
     {
-        std::cerr << "Usage: $ " << argv[0] << " /path/to/xml\n";
+        ::help_messages();
         return EXIT_FAILURE;
     }
 
