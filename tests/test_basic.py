1e14d549e09bc01668d6e2d941a77495d606d9be
dsw7 <dds3443@hotmail.com>
2024-05-15 01:32:33 -0700

    Add stdout and stderr formatting to tests

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,5 +1,6 @@
 from os import EX_OK
 from subprocess import run
+from colorama import Fore, Style
 from pytest import mark
 
 EX_FAIL = 1
@@ -18,16 +19,44 @@ INVALID_FILES = [
     "tests/xml/test_html_input.xml",
     "tests/xml/test_non_nmap.xml",
 ]
+PREFIX_STDOUT = Fore.GREEN + "stdout >>> " + Style.RESET_ALL
+PREFIX_STDERR = Fore.RED + "stderr >>> " + Style.RESET_ALL
 
 
-def test_help() -> None:
+def print_stdout(raw_stdout: str) -> None:
+    if not raw_stdout:
+        return
+
+    for line in raw_stdout.split("\n"):
+        print(PREFIX_STDOUT + line)
+
+
+def print_stderr(raw_stderr: str) -> None:
+    if not raw_stderr:
+        return
+
+    for line in raw_stderr.split("\n"):
+        print(PREFIX_STDERR + line)
+
+
+def test_help(capfd) -> None:
     process = run(["build/elo", "--help"])
     assert process.returncode == EX_FAIL
 
+    output = capfd.readouterr()
+    print()
+    print_stdout(output.out)
+    print_stderr(output.err)
 
-def test_help_shorthand() -> None:
+
+def test_help_shorthand(capfd) -> None:
     process = run(["build/elo", "-h"])
     assert process.returncode == EX_FAIL
+
+    output = capfd.readouterr()
+    print()
+    print_stdout(output.out)
+    print_stderr(output.err)
 
 
 def test_help_memory() -> None:
